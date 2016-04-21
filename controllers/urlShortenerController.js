630301ea1552d5e8460ec00e8c9ce71d841f72e4
ric mclaughlin <ric@mclaughlin.today>
2016-04-21 07:18:51 -0700

    add improvements

--- controllers/urlShortenerController.js
+++ controllers/urlShortenerController.js
@@ -1,12 +1,24 @@
 var urlShortenerController = function (timeStamp) {
-  get = function (req, res) {
-    if (req.params.date) {
+  function get(req, res) {
+    if (req.params.url) {
       res.status(201);
-      res.send('yeah');
+      res.send(makeid(3));
     } else {
       res.render('../index.html');
     }
-  };
+  }
+
+  function makeid(lengthOfString) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < lengthOfString; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+  }
+
   return {
     get: get
   };
--- routes/urlShortenerRoute.js
+++ routes/urlShortenerRoute.js
@@ -3,7 +3,7 @@ var routes = function (url) {
   var urlShortenerRouter = express.Router();
   var urlShortenerController = require('../controllers/urlShortenerController')(url);
 
-  urlShortenerRouter.route('/:date')
+  urlShortenerRouter.route('/:url')
     .get(urlShortenerController.get);
 
   return urlShortenerRouter;
